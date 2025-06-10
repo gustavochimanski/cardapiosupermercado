b09f5e1ccfbc1eb5bf0b9fd1ca4f48d4f3f1cfde
Gustavo chimanski antunes <gustavochimanski@outlook.com.br>
2025-06-10 20:25:24 -0300

    ...

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,11 @@
 // src/components/Footer/FooterComponent.tsx
 
-import { Home, Tag, List, ClipboardPen } from "lucide-react";
+import { Home, Tag, List, ClipboardPen, Apple } from "lucide-react";
 import Link from "next/link";
 
 const FooterComponent = () => {
   return (
-    <footer className=" fixed bottom-0 left-0 w-full bg-muted rounded-t text-primary flex justify-around items-center h-12 shadow-[0_-2px_8px_rgba(0,0,0,0.08)] z-50">
+    <footer className=" fixed bottom-0 left-0 w-full bg-primary rounded-t text-muted flex justify-around items-center h-12 shadow-[0_-2px_8px_rgba(0,0,0,0.08)] z-50">
       {/* Início */}
       <Link href="/" className="flex flex-col items-center gap-1 hover:text-secondary transition ">
         <Home size={18} />
@@ -15,6 +15,11 @@ const FooterComponent = () => {
       <Link href="/promocoes" className="flex flex-col items-center gap-1 hover:text-secondary transition">
         <Tag size={18} />
         <span className="text-xs">Promoções</span>
+      </Link>
+      {/* Categorias */}
+      <Link href="/categorias" className="flex flex-col items-center gap-1 hover:text-secondary transition">
+        <Apple size={18} />
+        <span className="text-xs">Categorias</span>
       </Link>
       {/* Pedidos */}
       <Link href="/pedidos" className="flex flex-col items-center gap-1 hover:text-secondary transition">
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,12 @@
 
 import React from 'react';
 import { SearchBarComponent } from './Search';
+import { MapPinCheck } from 'lucide-react';
 
 const HeaderComponent = () => {
   return (
-    <header className="w-full flex flex-col items-center p-4 pt-2 shadow-md sticky top-0 z-50 rounded-b bg-background">
-        <p className='font-semibold text-gray-500 text-sm '>Rua antonio de Oliveira Gago 83 </p>
+    <header className="w-full flex flex-col items-center p-4 pt-2 shadow-md sticky top-0 z-50 rounded-b bg-background gap-1">
+        <p className='font-semibold text-gray-500 text-sm flex items-center gap-1'><MapPinCheck size={15}/>Rua antonio de Oliveira Gago 83 </p>
         <SearchBarComponent/>
     </header>
   );
