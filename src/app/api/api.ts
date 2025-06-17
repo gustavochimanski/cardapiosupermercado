0f676a966f1ec7ae3488a35d692444822b86ff5e
Gustavo chimanski antunes <gustavochimanski@outlook.com.br>
2025-06-16 21:23:31 -0300

    .

--- src/app/api/api.ts
+++ src/app/api/api.ts
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http:/69.62.93.161:8000", // se mudar para produção, troca aqui
+  baseURL: "http://69.62.93.161:8000", // se mudar para produção, troca aqui
   headers: {
     "Content-Type": "application/json",
   },
